b5569772eb12ce8a68d7af757c9b3e1d2242db38
Grzegorz Lason <lasongrzegorz@gmail.com>
2020-04-01 10:16:09 +0200

    Change to url namespacing with app_name

--- shop/urls.py
+++ shop/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from .views import index, shop_view, contact_view, about_view
 from carts.views import add_to_cart
 
+app_name = 'shop'
+
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='home'),
     path('shop/', shop_view, name='shop'),
-    path('shop/add_to_cart/<int:product_id>/', add_to_cart,
-         name='add_to_cart'),
+    path('shop/add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/', include('carts.urls')),
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
