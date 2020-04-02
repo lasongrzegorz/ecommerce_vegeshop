a71e9f7d3ba44dfae35df3aa20a8275e80cc2531
Grzegorz Lason <lasongrzegorz@gmail.com>
2020-04-02 13:40:18 +0200

    Add and server checkout view on cart page

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from carts.models import Cart
+from .models import Order
 
-# Create your views here.
+
+def checkout(request):
+
+	try:
+		the_id = request.session['cart_id']
+		cart = Cart.objects.get(id=the_id)
+	except Cart.DoesNotExist:
+		the_id = None
+		return redirect(reverse('shop:carts:cart_view'))
+
+	# returns a tuple of (OrderObject, True/False)
+	new_order, created = Order.objects.get_or_create(cart=cart)
+	if created:
+		new_order.order_id = 'random_id'
+		new_order.save()
+
+	if new_order.status == 'Finished':
+		del request.session['cart_id']
+		return redirect(reverse('shop:carts:cart_view'))
+
+	context = {'cart': cart}
+	template = 'shop/pages/cart.html'
+
+	return render(request, template, context)
--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import index, shop_view, contact_view, about_view
 from carts.views import add_to_cart
+from orders.views import checkout
 
 app_name = 'shop'
 
@@ -9,6 +10,7 @@ urlpatterns = [
     path('shop/', shop_view, name='shop'),
     path('shop/add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/', include('carts.urls')),
+    path('shop/checkout', checkout, name='checkout'),
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
 ]
