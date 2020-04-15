564bfe366d54f1edff3909dabcd818e9a4e4823b
Grzegorz Lason <lasongrzegorz@gmail.com>
2020-04-15 16:23:55 +0200

    Update requirements.txt; adding aws region

--- VegiesWeb/settings.py
+++ VegiesWeb/settings.py
@@ -128,8 +128,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 # to copy settings to heroku!!
 django_heroku.settings(locals())
 
@@ -147,3 +145,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'eu-north-1'
